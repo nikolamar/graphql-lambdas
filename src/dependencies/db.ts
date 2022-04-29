46b437c91971ba35ffef0ff2a6e04ae9596b9f42
nikolamar <nikolamar@gmail.com>
2022-04-30 01:09:53 +0200

    feat: reuse db connection

--- src/dependencies/db.ts
+++ src/dependencies/db.ts
@@ -3,20 +3,16 @@ import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@quick.075mz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const options = {
-  // @ts-ignore
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1
 };
 
-export const client = new MongoClient(uri, options);
+const connection = MongoClient.connect(uri, options);
 
 export async function getMongodbClient () {
-  const connection = MongoClient.connect(uri, options);
   return connection.then(client => client.db());
 }
-
-export const DB_NAME = process.env.DB_NAME;
 
 export const toId = (key: string | ObjectId | unknown) => {
   if (typeof key === "string" && key.length === 24 && ObjectId.isValid(key)) {
