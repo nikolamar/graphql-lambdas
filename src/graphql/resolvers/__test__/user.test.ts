9fe52c6b912a210e57c751d928a0c88a2173e523
nikolamar <nikolamar@gmail.com>
2022-05-07 23:26:10 +0200

    feat: improve tests

--- src/graphql/resolvers/__test__/user.test.ts
+++ src/graphql/resolvers/__test__/user.test.ts
@@ -1,33 +1,30 @@
 import fs from "fs";
 import path from "path";
-import { authenticator } from "otplib";
 import { CLIENT_ID } from "/opt/configs/cognito";
 import { getMongodbConnectionWithClient } from "/opt/utils/db";
 import { ApolloTestServer, mockRequestOptions } from "../../utils/server-test";
 import type { Challenge, Tenant, User } from "../../generated";
 
+const usersQuery = fs.readFileSync(path.resolve(__dirname, "../../queries/users.graphql"), "utf8");
 const mfaStatusQuery = fs.readFileSync(path.resolve(__dirname, "../../queries/mfa-status.graphql"), "utf8");
-const mfaStatusUrlQuery = fs.readFileSync(path.resolve(__dirname, "../../queries/mfa-auth-url.graphql"), "utf8");
 const createTenantMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/create-tenant.graphql"), "utf8");
 const createUserMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/create-user.graphql"), "utf8");
 const userPasswordAuthMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/user-password-auth.graphql"), "utf8");
 const challengeNewPasswordMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/challenge-new-password.graphql"), "utf8");
-const validateMfaCodeMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/validate-mfa-code.graphql"), "utf8");
-const setUserMfaPreferenceMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/set-user-mfa-preference.graphql"), "utf8");
 const deleteUserMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/delete-user.graphql"), "utf8");
 const deleteTenantMutation = fs.readFileSync(path.resolve(__dirname, "../../mutations/delete-tenant.graphql"), "utf8");
 
-describe("sign-up", () => {
+describe("user resolver", () => {
   let user: User;
   let tenant: Tenant;
   let challenge: Challenge;
   let idToken: string;
   let accessToken: string;
-  let verificationCode: string;
+  const usersList: User[] = [];
 
   let server: ApolloTestServer;
 
-  const adminEmail = "admin.test+0@test.com";
+  const adminEmail = "admin.test+1@test.com";
 
   beforeAll(async () => {
     const [connection, client] = await getMongodbConnectionWithClient();
@@ -42,18 +39,22 @@ describe("sign-up", () => {
     const response = await server.test(createTenantMutation, {
       variables: { 
         input: {
-          name: "Admin Tenant 0",
+          name: "Admin Tenant 1",
           status: "active",
           color: "#ffffff",
-          accentColor: "#ffffff"
+          accentColor: "#cccccc"
         }
       }
     });
     expect(response.errors).toBeUndefined();
+    expect(response.data.createTenant.name).toEqual("Admin Tenant 1");
+    expect(response.data.createTenant.status).toEqual("active");
+    expect(response.data.createTenant.color).toEqual("#ffffff");
+    expect(response.data.createTenant.accentColor).toEqual("#cccccc");
     tenant = response.data.createTenant;
   });
   
-  test("create user", async () => {
+  test("create admin user", async () => {
     const response = await server.test(createUserMutation, {
       variables: {
         input: {
@@ -69,7 +70,7 @@ describe("sign-up", () => {
     expect(user).not.toBeUndefined();
   });
 
-  test("user password auth challenge response", async () => {
+  test("admin user password auth challenge response", async () => {
     const response = await server.test(userPasswordAuthMutation, {
       variables: {
         clientId: CLIENT_ID,
@@ -81,7 +82,7 @@ describe("sign-up", () => {
     expect(response.data.userPasswordAuth.challengeName).toEqual("NEW_PASSWORD_REQUIRED");
   });
 
-  test("challenge new password", async () => {
+  test("admin user challenge new password", async () => {
     const response = await server.test(challengeNewPasswordMutation, {
       variables: {
         clientId: CLIENT_ID,
@@ -96,7 +97,7 @@ describe("sign-up", () => {
     expect(accessToken).not.toBeUndefined();
   });
 
-  test("user password auth tokens response", async () => {
+  test("admin user password auth tokens response", async () => {
     const response = await server.test(userPasswordAuthMutation, {
       variables: {
         clientId: CLIENT_ID,
@@ -110,7 +111,7 @@ describe("sign-up", () => {
     expect(accessToken).not.toBeUndefined();
   });
 
-  test("mfa status false", async () => {
+  test("admin user mfa status false", async () => {
     mockRequestOptions.headers = {
       IdToken: idToken,
       AccessToken: accessToken
@@ -119,50 +120,58 @@ describe("sign-up", () => {
     expect(response.data.mfaStatus).toEqual(false);
   });
 
-  test("mfa auth url verification code", async () => {
-    const response = await server.test(mfaStatusUrlQuery, { variables: {} });
-    const secret = response.data.mfaAuthUrl.split("secret=")[1];
-    verificationCode = authenticator.generate(secret);
-    expect(verificationCode).not.toBeUndefined();
+  test("create many users", async () => {
+    // count and admin user that is already created
+    for (let index = 1; index < 14; index++) {
+      const userEmail = `user+${index}@test.com`;
+      const userResponse = await server.test(createUserMutation, {
+        variables: {
+          input: {
+            email: userEmail,
+            password: "Password01!",
+            role: "user",
+            tenantId: tenant._id,
+          },
+          skipCognito: true
+        }
+      });
+
+      expect(userResponse.errors).toBeUndefined();
+      expect(userResponse.data.createUser.email).toEqual(userEmail);
+      expect(userResponse.data.createUser.role).toEqual("user");
+      expect(userResponse.data.createUser.tenantId).toEqual(tenant._id);
+      expect(userResponse.data.createUser.tenant.name).toEqual("Admin Tenant 1");
+      usersList.push(userResponse.data.createUser);
+    }
   });
 
-  test("validate mfa code", async () => {
-    const response = await server.test(validateMfaCodeMutation, {
-      variables: {
-        verificationCode,
-      }
+  test("users descending pagination, cursors check", async () => {
+    const users1 = await server.test(usersQuery, { variables: { first: 5, order: "desc", sortBy: "email" } });
+    const users2 = await server.test(usersQuery, { variables: { first: 5, order: "desc", sortBy: "email", after: users1.data.users.pageInfo.endCursor } });
+    const users3 = await server.test(usersQuery, { variables: { first: 5, order: "desc", sortBy: "email", after: users2.data.users.pageInfo.endCursor } });
+    const allUsers = await server.test(usersQuery, { variables: { first: 50, order: "desc", sortBy: "email" } });
+    expect(users2.data.users.edges[0].node).toEqual(allUsers.data.users.edges[5].node);
+    expect(users3.data.users.edges[0].node).toEqual(allUsers.data.users.edges[10].node);
+    expect(users1.data.users.pageInfo.hasNextPage).toEqual(true);
+    expect(users2.data.users.pageInfo.hasNextPage).toEqual(true);
+    expect(users3.data.users.pageInfo.hasNextPage).toEqual(false);
+  });
+
+  test("delete many users", async () => {
+    usersList.forEach(async user => {
+      const response = await server.test(deleteUserMutation, {
+        variables: {
+          where: {
+            _id: { _eq: user._id }
+          },
+          skipCognito: true
+        }
+      });
+      expect(response.errors).toBeUndefined();
     });
-    expect(response.errors).toBeUndefined();
   });
-
-  test("enable mfa", async () => {
-    const response = await server.test(setUserMfaPreferenceMutation, {
-      variables: {
-        mfa: true,
-      }
-    });
-    expect(response.data.setUserMfaPreference).toEqual(true);
-    expect(response.errors).toBeUndefined();
-  });
-
-  test("mfa status true", async () => {
-    const response = await server.test(mfaStatusQuery, { variables: {} });
-    expect(response.data.mfaStatus).toEqual(true);
-  });
-
-  test("user password auth challenge software token mfa", async () => {
-    const response = await server.test(userPasswordAuthMutation, {
-      variables: {
-        clientId: CLIENT_ID,
-        username: adminEmail,
-        password: "NewPassword01!",
-      }
-    });
-    challenge = response.data.userPasswordAuth;
-    expect(response.data.userPasswordAuth.challengeName).toEqual("SOFTWARE_TOKEN_MFA");
-  });
-
-  test("delete user", async () => {
+  
+  test("admin user delete", async () => {
     const response = await server.test(deleteUserMutation, {
       variables: {
         where: {
@@ -173,7 +182,7 @@ describe("sign-up", () => {
     expect(response.errors).toBeUndefined();
   });
 
-  test("delete tenant", async () => {
+  test("admin user delete tenant", async () => {
     const response = await server.test(deleteTenantMutation, {
       variables: {
         where: {
