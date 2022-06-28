3849285a61246f15066437c7feb7b755a4b92f7a
Nikola Marjanovic <nikolamar@gmail.com>
2022-06-28 17:31:06 +0200

    feat: reduce node_modules

--- src/graphql/plugins/logger.ts
+++ src/graphql/plugins/logger.ts
@@ -1,16 +1,5 @@
-import clonedeep from "lodash.clonedeep";
-import { pino } from "/opt/utils/pino";
-import { rndletters } from "/opt/utils/nanoid";
-import { getSanitizedResponse } from "/opt/utils/sanitize";
-
 export const logger = {
   async requestDidStart (ctx) {
-    ctx.logger = pino.child({ requestId: rndletters() });
-    ctx.logger.info({
-      operationName: ctx.request.operationName,
-      query: ctx.request.query,
-      variables: ctx.request.variables,
-    });
     return {
       async didEncounterErrors (ctx) {
         ctx?.errors?.forEach(err => ctx.logger.error("Error", err, {
@@ -21,10 +10,7 @@ export const logger = {
       },
       async willSendResponse (ctx) {
         if (!ctx?.errors) {
-          const fieldsNotToSend = [];
-          
-          const sanitizedResponse = getSanitizedResponse(clonedeep(ctx)?.response, fieldsNotToSend);
-          ctx.logger.info(sanitizedResponse);
+          console.dir(ctx?.response);
         }
       },
     };
