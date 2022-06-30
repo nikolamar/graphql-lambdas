dca54335516673d9f3db2983082a1ffbd0e433b7
nikolamar <nikolamar@gmail.com>
2022-06-30 11:21:28 +0200

    feat: faster builds

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -2,10 +2,6 @@
 
 echo "Building the project with sam-cli and new beta features"
 
-cd connect      && npm ci && cd ..
-cd default      && npm ci && cd ..
-cd dependencies && npm ci && cd ..
-cd disconnect   && npm ci && cd ..
-cd graphql      && npm ci && cd ..
+npm install -g esbuild
 
 sam build --beta-features
