ee312547702a988bdffc178f9739d27cc9c0b962
Nikola Marjanovic <nikolamar@gmail.com>
2022-07-01 16:23:19 +0200

    feat: shell copy schemas

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "Building the project with sam-cli and new beta features"
+MY_DIR=$(realpath $0)
+CICD_DIR=$(dirname $MY_DIR)
+PROJECT_ROOT=$(dirname $CICD_DIR)
+
+echo "Install globally esbuild"
 
 npm install -g esbuild
 
+echo "Building the project with sam-cli and new beta features"
+
 sam build --beta-features
+
+echo "Copy schemas"
+
+cp -r "${PROJECT_ROOT}/lambdas/graphql/src/schemas" "${PROJECT_ROOT}/.aws-sam/build/GraphqlFunction"
