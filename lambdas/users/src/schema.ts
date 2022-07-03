f701319a9014d088ab5c4bdad8a91433fc98d06d
nikolamar <nikolamar@gmail.com>
2022-07-03 13:03:19 +0200

    fix: schema imports

--- lambdas/users/src/schema.ts
+++ lambdas/users/src/schema.ts
@@ -1,10 +1,9 @@
-import { join } from "path";
 import { mergeTypeDefs } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { resolvers } from "./resolvers";
 
-const typesArray = loadFilesSync(join(__dirname, "/opt/schemas"));
+const typesArray = loadFilesSync("/opt/schemas");
 const typeDefs = mergeTypeDefs(typesArray);
 
 export const schemaWithResolvers = makeExecutableSchema({
